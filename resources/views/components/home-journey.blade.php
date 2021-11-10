858f83dd026a138a2d872e4651f5b619127e59f0
devina.godjali <devinagodjali@gmail.com>
2021-11-10 14:42:09 +0700

    our jouney mobile view

--- resources/views/components/home-journey.blade.php
+++ resources/views/components/home-journey.blade.php
@@ -1,12 +1,12 @@
 <section class="Flex JustifyContent-center MarginTop-2xlarge">
     <div class="HomeJourney Padding Flex Flex-column AlignItems-center BorderBox">
-        <div class="Montserrat-bold  Font-24 TextAlign-center Padding"> Our Journey </div>
+        <div class="Montserrat-bold  Font-24 TextAlign-center Padding" style="padding-bottom: 0;"> Our Journey </div>
         <hr class="BorderBottom" />
 
         <div class="HomeJourney-content Flex JustifyContent-center MarginTop-2xlarge"> 
             <div class="Padding-r Height Flex-1"> 
                 <div class="Font-54 Montserrat-bold Height-100"> 1992 </div> 
-                <div class="HomeJourney-subText MarginTop-xsmall Roboto Height-100"> 
+                <div class="HomeJourney-subText MarginTop-xsmall Roboto Height-100 Font-14"> 
                     Starting From a home industry as a lacqueer table manufacture for food courts/ restaurants.
                 </div>
             </div>
@@ -15,7 +15,7 @@
 
             <div class="Padding-r Height Flex-1"> 
                 <div class="Font-54 Montserrat-bold Height-100"> > 200 </div> 
-                <div class="HomeJourney-subText MarginTop-xsmall Roboto Height-100">
+                <div class="HomeJourney-subText MarginTop-xsmall Roboto Height-100 Font-14">
                     Product variant were made and will continue to innovate with the best quality
                 </div>
             </div>
@@ -24,7 +24,7 @@
 
             <div class="Padding-r Height Flex-1">
                 <div class="Font-54 Montserrat-bold Height-100"> 34 </div> 
-                <div class="HomeJourney-subText MarginTop-xsmall Roboto Height-100"> 
+                <div class="HomeJourney-subText MarginTop-xsmall Roboto Height-100 Font-14"> 
                     Provinces in Indonesia has been reached with safe delivery. 
                 </div>
             </div>
@@ -32,39 +32,31 @@
 
         <div class="HomeJourney-circle Padding Flex JustifyContent-evenly AlignItems-center MarginBottom">
             <div class="Flex AlignItems-center">
-                <div class="Img">
-                    <img src="/assets/png/icon-01.png" alt="Icon 1">
-                </div>
+                <img class="Img" src="/assets/png/icon-01.png" alt="Icon 1">
                 <div class="Text">
-                    <div class="Montserrat-light Font-24"> Excellence </div>
-                    <div class="Montserrat-medium Font-24"> Service </div>
+                    <div class="Montserrat-light FontM"> Excellence </div>
+                    <div class="Montserrat-medium FontM"> Service </div>
                 </div>
             </div>
             <div class="Flex AlignItems-center">
-                <div class="Img">
-                    <img src="/assets/png/icon-02.png" alt="Icon 2">
-                </div>
+                <img class="Img" src="/assets/png/icon-02.png" alt="Icon 2">
                 <div class="Text">
-                    <div class="Montserrat-light Font-24"> Product </div>
-                    <div class="Montserrat-medium Font-24"> Inovation </div>
+                    <div class="Montserrat-light FontM"> Product </div>
+                    <div class="Montserrat-medium FontM"> Inovation </div>
                 </div>
             </div>
             <div class="Flex AlignItems-center">
-                <div class="Img">
-                    <img src="/assets/png/icon-03.png" alt="Icon 3">
-                </div>
+                <img class="Img" src="/assets/png/icon-03.png" alt="Icon 3">
                 <div class="Text">
-                    <div class="Montserrat-light Font-24"> Best Quality </div>
-                    <div class="Montserrat-medium Font-24"> Of Product </div>
+                    <div class="Montserrat-light FontM"> Best Quality </div>
+                    <div class="Montserrat-medium FontM"> Of Product </div>
                 </div>
             </div>
             <div class="Flex AlignItems-center">
-                <div class="Img">
-                    <img src="/assets/png/icon-04.png" alt="Icon 4">
-                </div>
+                <img class="Img" src="/assets/png/icon-04.png" alt="Icon 4">
                 <div class="Text">
-                    <div class="Montserrat-light Font-24"> Customer </div>
-                    <div class="Montserrat-medium Font-24"> Satisfaction </div>
+                    <div class="Montserrat-light FontM"> Customer </div>
+                    <div class="Montserrat-medium FontM"> Satisfaction </div>
                 </div>
             </div>
         </div>
